ec29e7e6e81964dac1228d497cc37b8ba516fe72
mertsimsek <mertsimsek1992@gmail.com>
2018-01-24 22:14:17 +0300

    Wrap actual view with drawerlayout.

--- meowlib/src/main/java/iammert/com/meowlib/ui/DrawerAttacher.kt
+++ meowlib/src/main/java/iammert/com/meowlib/ui/DrawerAttacher.kt
@@ -3,6 +3,7 @@ package iammert.com.meowlib.ui
 import android.app.Activity
 import android.support.v4.widget.DrawerLayout
 import android.view.Gravity
+import android.view.View
 import android.widget.FrameLayout
 import android.widget.LinearLayout
 import iammert.com.meowlib.R
@@ -13,16 +14,28 @@ import iammert.com.meowlib.R
 class DrawerAttacher constructor(private val activity: Activity?) {
 
     fun attach() {
+        val content: FrameLayout = activity!!.window.decorView.findViewById<View>(android.R.id.content) as FrameLayout
+        val actualView = content.getChildAt(0)
+
+        content.removeAllViews()
+
         val drawerLayout = DrawerLayout(activity)
+        if (actualView != null) {
+            drawerLayout.addView(actualView)
+        }
+
         val layoutParamDrawerLayout = DrawerLayout.LayoutParams(
                 LinearLayout.LayoutParams.MATCH_PARENT,
                 LinearLayout.LayoutParams.MATCH_PARENT)
         layoutParamDrawerLayout.gravity = Gravity.END
+
         val frameLayout = FrameLayout(activity)
         frameLayout.id = R.id.container
         frameLayout.layoutParams = layoutParamDrawerLayout
         drawerLayout.addView(frameLayout)
-        activity!!.addContentView(drawerLayout, layoutParamDrawerLayout)
+
+        activity.addContentView(drawerLayout, layoutParamDrawerLayout)
+
         activity.fragmentManager.beginTransaction().add(R.id.container, MeowFragment()).commit()
     }
 }
